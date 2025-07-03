58af9b46b0d44f712d3941d197bbf20cfaaa269f
gifted124 <giftedmohammed500@gmail.com>
2025-07-02 22:57:33 -1100

    added BiDnaLoading and Dashboard components

--- src/compunet/BiDnaLoading.jsx
+++ src/compunet/BiDnaLoading.jsx
@@ -1,33 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { BiDna } from "react-icons/bi";
-import Navbar from "./compunet/Navbar";
 
-
-function App() {
+const BiDnaLoading = () => {
   const [showLoader, setShowLoader] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowLoader(false);
-    }, 4000);
+    }, 4000); // 4 seconds
 
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <>
-      {showLoader ? (
+      {showLoader && (
         <div className="flex flex-col items-center justify-center h-screen bg-white">
+          {/* Bouncing Icon */}
           <BiDna className="text-6xl text-blue-600 animate-bounce" />
+
+          {/* Loading Line */}
           <div className="mt-4 w-48 h-1 bg-gray-300 relative overflow-hidden rounded-full">
             <div className="absolute inset-0 bg-blue-600 animate-loading-bar"></div>
           </div>
         </div>
-      ) : (
-        <Navbar />
       )}
     </>
   );
-}
+};
 
-export default App;
+export default BiDnaLoading;
